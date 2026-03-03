307692a078f78582e22efc8646053e212ea96c99
sishilalala <si.shi922@gmail.com>
2026-03-03 14:32:51 +0800

    📊 添加基金行业要闻板块

--- data-meta.js
+++ data-meta.js
@@ -1,7 +1,1 @@
-const FUND_DATA_META = {
-  "metadata": {
-    "updated_at": "2026-03-02 14:46:30",
-    "trading_date": "2026-03-02",
-    "total_funds_available": 19288
-  }
-};
+const FUND_DATA_META = {"metadata":{"updated_at":"2026-03-02 14:46:30","trading_date":"2026-03-02","total_funds_available":19288}};
